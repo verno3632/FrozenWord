c2031bc2a50abf500e5e1f7c96dd4905adcf7b8b
verno3632 <verno3632@pixiv.com>
2021-12-04 17:12:00 +0900

    chipを実装

--- app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddHabbitScreen.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddHabbitScreen.kt
@@ -1,13 +1,19 @@
 package jp.developer.retia.frozenword.ui.addHabbit
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.developer.retia.frozenword.R
 import jp.developer.retia.frozenword.ui.theme.FrozenWordTheme
 import jp.developer.retia.frozenword.ui.viewmodel.AddHabbitUiState
 
@@ -66,4 +72,27 @@ fun FirstPane(
     }
 }
 
+@Composable
+fun Chip(
+    title: String,
+    onSelectedCategoryChanged: (String) -> Unit,
+) {
+    Surface(
+        modifier = Modifier
+            .padding(end = 8.dp, bottom = 8.dp)
+            .clickable { onSelectedCategoryChanged(title) },
+        elevation = 8.dp,
+        shape = RoundedCornerShape(16.dp),
+        color = colorResource(R.color.purple_500)
+    ) {
+        Row {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
 
