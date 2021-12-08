784faffd28f4924cc6692482eff358e508b727bb
verno3632 <verno3632@pixiv.com>
2021-12-08 09:52:33 +0900

    目標を追加したら画面を戻る

--- app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddHabbitActivity.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddHabbitActivity.kt
@@ -11,16 +11,21 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dagger.hilt.android.AndroidEntryPoint
 import jp.developer.retia.frozenword.ui.theme.FrozenWordTheme
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @OptIn(InternalCoroutinesApi::class)
 @AndroidEntryPoint
 class AddHabbitActivity : ComponentActivity() {
 
-    private val mainViewModel: AddHabbitViewModel by viewModels()
+    private val addHabbitViewModel: AddHabbitViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +35,16 @@ class AddHabbitActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     AddHabbitScreen()
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                addHabbitViewModel.events.collect { event ->
+                    when (event) {
+                        AddHabbitEvent.Back -> onBackPressed()
+                    }
                 }
             }
         }
