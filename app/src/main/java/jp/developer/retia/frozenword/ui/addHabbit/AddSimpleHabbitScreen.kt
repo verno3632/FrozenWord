88e81d59b8aa98d5e031128e23dd2a741db1298c
verno3632 <retia.verno@gmail.com>
2022-01-02 14:58:13 +0900

    Update app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddSimpleHabbitScreen.kt

--- app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddSimpleHabbitScreen.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/addHabbit/AddSimpleHabbitScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import jp.developer.retia.frozenword.ui.theme.FrozenWordTheme
 
 @Preview(showBackground = true)
 @Composable
