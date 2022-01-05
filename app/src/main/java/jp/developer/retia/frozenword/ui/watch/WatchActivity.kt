a8077c9591b54633a6d4ea86547c34e6101da575
verno3632 <verno3632@pixiv.com>
2022-01-05 19:45:37 +0900

    メモ入力したら戻る

--- app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchActivity.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchActivity.kt
@@ -12,12 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.core.os.bundleOf
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import jp.developer.retia.frozenword.extension.lazyWithExtras
 import jp.developer.retia.frozenword.model.Habbit
+import jp.developer.retia.frozenword.ui.addHabbit.AddHabbitEvent
 import jp.developer.retia.frozenword.ui.theme.FrozenWordTheme
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 @OptIn(InternalCoroutinesApi::class)
 @AndroidEntryPoint
@@ -39,6 +44,16 @@ class WatchActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     WatchScreen(watchViewModel)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                watchViewModel.events.collect { event ->
+                    when (event) {
+                        WatchEvent.Back -> onBackPressed()
+                    }
                 }
             }
         }
--- app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchViewModel.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchViewModel.kt
@@ -10,9 +10,13 @@ import java.util.Date
 import jp.developer.retia.frozenword.model.Habbit
 import jp.developer.retia.frozenword.model.Log
 import jp.developer.retia.frozenword.repository.HabbitRepository
+import jp.developer.retia.frozenword.ui.addHabbit.AddHabbitEvent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
@@ -28,6 +32,9 @@ class WatchViewModel @AssistedInject constructor(
 
     private val _uiState = MutableStateFlow<WatchUiState>(WatchUiState.NotLoaded)
     val uiState: StateFlow<WatchUiState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<WatchEvent>()
+    val events: SharedFlow<WatchEvent> = _events.asSharedFlow()
 
     init {
         viewModelScope.launch(ioDispatcher) {
@@ -47,6 +54,7 @@ class WatchViewModel @AssistedInject constructor(
         viewModelScope.launch(ioDispatcher) {
             habbitRepository.updateMessage(logId, message)
 
+            _events.emit(WatchEvent.Back)
         }
     }
 
@@ -67,4 +75,8 @@ sealed class WatchUiState {
     object NotLoaded : WatchUiState()
     data class Loaded(val habbit: Habbit) : WatchUiState()
     data class EditMemo(val logId: Int, val message: String) : WatchUiState()
+}
+
+sealed class WatchEvent {
+    object Back : WatchEvent()
 }
