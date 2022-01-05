d88c4268084688d7ac7ed031d72397c55629375d
verno3632 <verno3632@pixiv.com>
2022-01-05 19:46:14 +0900

    remove unused import

--- app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchScreen.kt
+++ app/src/main/java/jp/developer/retia/frozenword/ui/watch/WatchScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import jp.developer.retia.frozenword.model.Log
 import jp.developer.retia.frozenword.ui.theme.FrozenWordTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
